26f9bef74e72843603b0546b4de660d0ce8df8bf
Enator18 <62544405+Enator18@users.noreply.github.com>
2024-01-26 12:25:03 -0500

    removed extraneous model

--- game_main.cpp
+++ game_main.cpp
@@ -68,20 +68,12 @@ void SlopeGame::init()
 		{glm::vec3(0.5, 0.5, 0), glm::vec3(1.0, 0.0, 0.0), glm::vec2(0.5, 0.5)}
 	};
 
-	std::vector<Vertex> vertices2 = {
-		{glm::vec3(-0.5, -0.5, 0), glm::vec3(1.0, 0.0, 0.0), glm::vec2(-0.5, -0.5)},
-		{glm::vec3(0.5, -0.5, 0), glm::vec3(1.0, 0.0, 0.0), glm::vec2(0.5, -0.5)},
-		{glm::vec3(0.5, 0.5, 0), glm::vec3(1.0, 0.0, 0.0), glm::vec2(0.5, 0.5)}
-	};
-
 	std::vector<uint32_t> indices = { 0, 1, 2 };
 	triangle = { vertices, indices };
-	triangle2 = { vertices2, indices };
 	renderer.uploadMesh(triangle);
-	renderer.uploadMesh(triangle2);
 
 	MeshInstance instance = { &triangle, glm::vec3(0, 0, 0), glm::quat(), glm::vec3(1, 1, 1) };
-	MeshInstance instance2 = { &triangle2, glm::vec3(0, 0, -0.5), glm::quat(), glm::vec3(1, 1, 1) };
+	MeshInstance instance2 = { &triangle, glm::vec3(0, 0, -0.5), glm::quat(), glm::vec3(1, 1, 1) };
 	instance.transform = instance.getTransformMatrix();
 	instance2.transform = instance2.getTransformMatrix();
 
